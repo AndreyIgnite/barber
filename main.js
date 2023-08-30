8f9c6ed86c2cf9066e3a34f7f09cf0fc348655af
Andrey <mozobretenie@gmail.com>
2023-08-30 23:31:44 +0300

    Модальное окно

--- main.js
+++ main.js
@@ -9,5 +9,17 @@ button_popover_on.addEventListener("click", function(e){
 button_popover_off.addEventListener("click", function(e){
     e.preventDefault();
     popover.classList.toggle('popover-show');
+});
+
+const button_login = document.querySelector("#button-login")
+const modal_window = document.querySelector(".modal-container")
+const button_close_modal = document.querySelector(".modal-close")
+
+button_login.addEventListener("click", function(e){
+    e.preventDefault()
+    modal_window.classList.add("modal-show")
 })
 
+button_close_modal.addEventListener("click", function(){
+    modal_window.classList.toggle("modal-show")
+})
